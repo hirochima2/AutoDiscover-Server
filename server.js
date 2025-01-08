0a69336e0a5196665289ef99b0cd923e3f13c9de
hirochima2 <99726382+hirochima2@users.noreply.github.com>
2025-01-08 01:35:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const carRoutes = require('./routes/carRoutes');
+require('dotenv').config();
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// PostgreSQL connection
+const pool = new Pool({
+  connectionString: process.env.POSTGRESQL_URI,
+});
+
+app.use('/api/cars', carRoutes(pool));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
